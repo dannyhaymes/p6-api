141aa0ba56f2661f00e84d564e915a658cd649c0
Danny Haymes <dannyhaymes94@gmail.com>
2023-06-13 14:45:26 +0100

    milestone 3 complete

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 
@@ -26,3 +27,45 @@ exports.signUp = (req, res, next) => {
         }
     );
 };
+
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email }).then(
+        (user) => {
+            if (!user) {
+                return res.status(401).json({
+                    error: new Error('User not found!')
+                });
+            }
+            bcrypt.compare(req.body.password, user.password).then(
+                (valid) => {
+                    if (!valid) {
+                        return res.status(401).json({
+                            error: new Error('Incorrect password!')
+                        });
+                    }
+                    const token = jwt.sign(
+                        { userId: user._id },
+                        'RANDOM_TOKEN_SECRET',
+                        { expiresIn: '24h' });
+
+                    res.status(200).json({
+                        userId: user._id,
+                        token: token
+                    });
+                }
+            ).catch(
+                (error) => {
+                    res.status(500).json({
+                        error: error.message || error
+                    });
+                }
+            );
+        }
+    ).catch(
+        (error) => {
+            res.status(500).json({
+                error: error.message || error
+            });
+        }
+    );
+}
--- back/routes/user.js
+++ back/routes/user.js
@@ -2,4 +2,5 @@ const userCtrl = require('../controllers/user');
 const express = require('express');
 const router = express.Router();
 router.post('/signUp', userCtrl.signUp);
+router.post('/login', userCtrl.login);
 module.exports = router;
